6bc123f2a2a9053a2feb182a1e7e6821618a7ce9
ch1ebak <92380391+ch1ebak@users.noreply.github.com>
2024-10-30 22:25:00 +0100

    Add files via upload

--- theme-switcher/themes/spacegray.sh
+++ theme-switcher/themes/spacegray.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
 # alacritty
-sed -i 's/tokyonight/spacegray/g' /home/$USER/.dotfiles/.config/alacritty/alacritty.toml
+sed -i 's@themes/*.*@themes/spacegray.toml"@g' /home/$USER/.dotfiles/.config/alacritty/alacritty.toml
 
 # btop
-sed -i 's/tokyonight/spacegray/g' /home/$USER/.dotfiles/.config/btop/btop.conf
+sed -i 's@themes/*.*@themes/spacegray.theme"@g' /home/$USER/.dotfiles/.config/btop/btop.conf
 
 # dunst
-sed -i 's/1a1b26/20242d/g' /home/$USER/.dotfiles/.config/dunst/dunstrc
-sed -i 's/a9b1d6/b3b8c3/g' /home/$USER/.dotfiles/.config/dunst/dunstrc
-sed -i 's/f7768e/b04b57/g' /home/$USER/.dotfiles/.config/dunst/dunstrc
-killall -e dunst & sleep 1; dunstify "hello!" &
+sed -i 's@background\ =\ "#*.*@background\ =\ "#20242d"@g' /home/$USER/.dotfiles/.config/dunst/dunstrc
+sed -i 's@foreground\ =\ "#*.*@foreground\ =\ "#b3b8c3"@g' /home/$USER/.dotfiles/.config/dunst/dunstrc
+sed -i 's@frame_color\ =\ "#*.*@frame_color\ =\ "#b04b57"@g' /home/$USER/.dotfiles/.config/dunst/dunstrc
+killall -e dunst & sleep 1; dunstify "Theme switched to Spacegray" &
 
 # neovim
-mv /home/$USER/.dotfiles/.config/nvim/lua/plugins/tokyo-night.lua /home/$USER/.dotfiles/.config/nvim/lua/plugins/tokyo-night.lua.bak
-mv /home/$USER/.dotfiles/.config/nvim/lua/plugins/nordic.lua.bak /home/$USER/.dotfiles/.config/nvim/lua/plugins/nordic.lua
-sed -i 's/tokyonight/nord/g' /home/kd/.dotfiles/.config/nvim/lua/plugins/lualine.lua
+sed -i "s@colorscheme\ *.*@colorscheme nordic'@g" /home/$USER/.dotfiles/.config/nvim/lua/core/options.lua
+sed -i "s@theme\ =\ *.*@theme\ =\ 'nord',@g" /home/$USER/.dotfiles/.config/nvim/lua/plugins/lualine.lua
 
 # qtile
-sed -i 's/tokyonight/spacegray/g' /home/$USER/.dotfiles/.config/qtile/config.py
+sed -i 's/themes.*\ import\ colors/themes.spacegray\ import\ colors/g' /home/$USER/.dotfiles/.config/qtile/config.py
+sed -i 's@wallpapers/*.*@wallpapers/spacegray.png",@g' /home/$USER/.dotfiles/.config/qtile/config.py
 
 # rofi
-sed -i 's/tokyonight/spacegray/g' /home/$USER/.dotfiles/.config/rofi/config.rasi
-
-# Xresources
-sed -i 's/tokyonight/spacegray/g' /home/$USER/.dotfiles/.Xresources
-xrdb -merge ~/.Xresources
+sed -i 's@themes/*.*@themes/spacegray"@g' /home/$USER/.dotfiles/.config/rofi/config.rasi
 
 # spicetify
-sed -i 's/TokyoNight/Spacegray/g' /home/$USER/.dotfiles/.config/spicetify/config-xpui.ini
+spicetify config current_theme text
+spicetify config color_scheme Spacegray
 spicetify apply
+
+# Startpage
+sed -i 's@assets/*.*@assets/style-spacegray.css"\ rel="stylesheet" />@g' /ssd/Projekty/orgmode-startpage/index.html
+
+# Xresources
+sed -i 's@xresources/*.*@xresources/spacegray"@g' /home/$USER/.dotfiles/.Xresources
+xrdb -merge ~/.Xresources
--- theme-switcher/themes/tokyonight.sh
+++ theme-switcher/themes/tokyonight.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
 # alacritty
-sed -i 's/spacegray/tokyonight/g' /home/$USER/.dotfiles/.config/alacritty/alacritty.toml
+sed -i 's@themes/*.*@themes/tokyonight.toml"@g' /home/$USER/.dotfiles/.config/alacritty/alacritty.toml
 
 # btop
-sed -i 's/spacegray/tokyonight/g' /home/$USER/.dotfiles/.config/btop/btop.conf
+sed -i 's@themes/*.*@themes/tokyonight.theme"@g' /home/$USER/.dotfiles/.config/btop/btop.conf
 
 # dunst
-sed -i 's/20242d/1a1b26/g' /home/$USER/.dotfiles/.config/dunst/dunstrc
-sed -i 's/b3b8c3/a9b1d6/g' /home/$USER/.dotfiles/.config/dunst/dunstrc
-sed -i 's/b04b57/f7768e/g' /home/$USER/.dotfiles/.config/dunst/dunstrc
-killall -e dunst & sleep 1; dunstify "hello!" &
+sed -i 's@background\ =\ "#*.*@background\ =\ "#1a1b26"@g' /home/$USER/.dotfiles/.config/dunst/dunstrc
+sed -i 's@foreground\ =\ "#*.*@foreground\ =\ "#a9b1d6"@g' /home/$USER/.dotfiles/.config/dunst/dunstrc
+sed -i 's@frame_color\ =\ "#*.*@frame_color\ =\ "#f7768e"@g' /home/$USER/.dotfiles/.config/dunst/dunstrc
+killall -e dunst & sleep 1; dunstify "Theme switched to Tokyo Night" &
 
 # neovim
-mv /home/$USER/.dotfiles/.config/nvim/lua/plugins/nordic.lua /home/$USER/.dotfiles/.config/nvim/lua/plugins/nordic.lua.bak
-mv /home/$USER/.dotfiles/.config/nvim/lua/plugins/tokyo-night.lua.bak /home/$USER/.dotfiles/.config/nvim/lua/plugins/tokyo-night.lua
-sed -i 's/nord/tokyonight/g' /home/kd/.dotfiles/.config/nvim/lua/plugins/lualine.lua
+sed -i "s@colorscheme\ *.*@colorscheme tokyonight'@g" /home/$USER/.dotfiles/.config/nvim/lua/core/options.lua
+sed -i "s@theme\ =\ *.*@theme\ =\ 'tokyonight',@g" /home/$USER/.dotfiles/.config/nvim/lua/plugins/lualine.lua
 
 # qtile
-sed -i 's/spacegray/tokyonight/g' /home/$USER/.dotfiles/.config/qtile/config.py
+sed -i 's/themes.*\ import\ colors/themes.tokyonight\ import\ colors/g' /home/$USER/.dotfiles/.config/qtile/config.py
+sed -i 's@wallpapers/*.*@wallpapers/tokyonight.png",@g' /home/$USER/.dotfiles/.config/qtile/config.py
 
 # rofi
-sed -i 's/spacegray/tokyonight/g' /home/$USER/.dotfiles/.config/rofi/config.rasi
-
-# Xresources
-sed -i 's/spacegray/tokyonight/g' /home/$USER/.dotfiles/.Xresources
-xrdb -merge ~/.Xresources
+sed -i 's@themes/*.*@themes/tokyonight"@g' /home/$USER/.dotfiles/.config/rofi/config.rasi
 
 # spicetify
-sed -i 's/Spacegray/TokyoNight/g' /home/$USER/.dotfiles/.config/spicetify/config-xpui.ini
+spicetify config current_theme text
+spicetify config color_scheme TokyoNight
 spicetify apply
+
+# Startpage
+sed -i 's@assets/*.*@assets/style-tokyonight.css"\ rel="stylesheet" />@g' /ssd/Projekty/orgmode-startpage/index.html
+
+# Xresources
+sed -i 's@xresources/*.*@xresources/tokyonight"@g' /home/$USER/.dotfiles/.Xresources
+xrdb -merge ~/.Xresources
