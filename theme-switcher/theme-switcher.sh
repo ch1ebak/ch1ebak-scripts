92724a55c115a25d84060221bef4d8a869fab319
ch1ebak <92380391+ch1ebak@users.noreply.github.com>
2024-10-31 21:38:03 +0100

    Add files via upload

--- theme-switcher/theme-switcher.sh
+++ theme-switcher/theme-switcher.sh
@@ -9,9 +9,19 @@ echo -e "
                                                                      
 "
 
-tokyonight () {
-    echo "Changing theme to $opt"
-    ./theme-switcher/themes/tokyonight.sh
+catppuccin () {
+  echo "Changing theme to $opt"
+  ./theme-switcher/themes/catppuccin.sh
+}
+
+dracula () {
+  echo "Changing theme to $opt"
+  ./theme-switcher/themes/dracula.sh
+}
+
+gruvbox () {
+  echo "Changing theme to $opt"
+  ./theme-switcher/themes/gruvbox.sh
 }
 
 spacegray () {
@@ -19,15 +29,23 @@ spacegray () {
     ./theme-switcher/themes/spacegray.sh
 }
 
+tokyonight () {
+  echo "Changing theme to $opt"
+  ./theme-switcher/themes/tokyonight.sh
+}
+
 while true; do
-    options=("Tokyo Night" "Spacegray" "Quit")
+    options=("Catppuccin" "Dracula" "Gruvbox" "Spacegray" "Tokyo Night" "Quit")
 
     echo "Choose an option: "
     select opt in "${options[@]}"; do
         case $REPLY in
-            1) tokyonight; break ;;
-            2) spacegray; break ;;
-            3) break 2 ;;
+            1) catppuccin; break ;;
+            2) dracula; break ;;
+            3) gruvbox; break ;;
+            4) spacegray; break ;;
+            5) tokyonight; break ;;
+            6) break 2 ;;
             *) echo "Invalid option $REPLY" >&2
         esac
     done
